a4d1d19f1adc323dec6b3b4d49febb4b1c7734c6
Tres Trantham <tres@trestrantham.com>
2013-11-24 09:21:07 -0500

    Reconfigure pair setup with github-auth

---
+++ bootstrap/osx/remote/pair.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+source ${BASH_SOURCE[0]%/*/*/*}/include.sh
+
+###############################################################################
+# e_header "Remote Pair"
+###############################################################################
+
+e_rocket "Creating aliases for Pair and pair"
+sudo dscl . -append /Users/$USER RecordName Pair pair
+
+e_rocket "Disabling SSH password authentication"
+sudo perl -pi -e 's/^#PasswordAuthentication/PasswordAuthentication/' /etc/sshd_config
+sudo perl -pi -e 's/^#ChallengeResponseAuthentication/ChallengeResponseAuthentication/' /etc/sshd_config
+sudo perl -pi -e 's/^ChallengeResponseAuthentication\s+yes/ChallengeResponseAuthentication no/' /etc/sshd_config
+
+e_rocket "Configuring SSH public keys"
+users=( albus522 bryckbost danielmorrison emilford \
+        ersatzryan gaffneyc jasonroelofs jcarpenter88 \
+        laserlemon manlycode pichot tbugai trestrantham )
+
+# GitHub seems to throttle these requests.
+# Try again later if there's any unexpected errors.
+for user in "${users[@]}"; do
+  e_arrow "Adding GitHub public keys for $user"
+  gh-auth add --users $user --command="$( which tmux ) attach -t pair"
+done
