d83c423904bfddd0eed90d6e311bb6a9faf841a2
Tres Trantham <tres@trestrantham.com>
2013-11-23 23:27:03 -0500

    Update tmux aliases to include both local and socket aliases

--- bash/aliases/tmux.bash
+++ bash/aliases/tmux.bash
@@ -1,6 +1,14 @@
-alias t='tmux -S ~/.tmux.socket'
+# Local tmux
+alias t='tmux'
+alias tn='t new-session -s'
 alias ta='t attach -t'
-alias tn='t new -s'
 alias tap='ta pair'
-alias tnp='ssh-agent tn pair'
 alias tls='t list-sessions'
+
+# Socket tmux
+alias T='tmux -S ~/.tmux.socket'
+alias Tn='T new -s'
+alias Ta='T attach -t'
+alias Tap='Ta pair'
+# alias Tnp='ssh-agent Tn pair'
+alias Tls='T list-sessions'
