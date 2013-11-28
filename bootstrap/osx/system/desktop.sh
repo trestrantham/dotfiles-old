f51d0b0346d830d334767335669b2ff2055f967a
Tres Trantham <tres@trestrantham.com>
2013-11-28 01:03:05 -0500

    Add Mavericks-compatible way to change desktop wallpapers for all screens

--- bootstrap/osx/system/desktop.sh
+++ bootstrap/osx/system/desktop.sh
@@ -8,4 +8,5 @@ e_header "Desktop"
 ###############################################################################
 
 e_rocket "Setting desktop background"
-defaults write com.apple.desktop Background '{default = {ImageFilePath = "/Library/Desktop Pictures/Shapes.jpg"; };}'
+# defaults write com.apple.desktop Background '{default = {ImageFilePath = "/Library/Desktop Pictures/Shapes.jpg"; };}'
+python $SCRIPT_PATH/config/set_desktops.py --path /Library/Desktop\ Pictures/Shapes.jpg
