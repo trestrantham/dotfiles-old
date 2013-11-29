09b92ef8af37222719831996f4c591e61279f6d6
Tres Trantham <tres@trestrantham.com>
2013-11-28 21:00:07 -0500

    Just install rubies

--- bootstrap/ruby/rubies.sh
+++ bootstrap/ruby/rubies.sh
@@ -5,26 +5,10 @@ source ${SCRIPT_PATH%/*}/include.sh
 
 read -p "Install ruby 1.9.3 with chruby? " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  ruby-install ruby 1.9.3
-
-  read -p "Set ruby 1.9.3 as default ruby? " -n 1 -r
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "chruby ruby-1.9.3" >> ~/.bash_profile
-  fi
-fi
+if [[ $REPLY =~ ^[Yy]$ ]]; then ruby-install ruby 1.9.3; fi
 
 echo
 
 read -p "Install ruby 2.0.0 with chruby? " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  ruby-install ruby 2.0.0
-
-  read -p "Set ruby 2.0.0 as default ruby? " -n 1 -r
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "chruby ruby-2.0.0" >> ~/.bash_profile
-  fi
-fi
+if [[ $REPLY =~ ^[Yy]$ ]]; then ruby-install ruby 2.0.0; fi
