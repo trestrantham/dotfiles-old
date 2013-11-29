67b9ea4d6029a2883258851e213075550d1479d5
Tres Trantham <tres@trestrantham.com>
2013-11-29 01:34:33 -0500

    Add libyaml formula

--- bootstrap/homebrew/brew.sh
+++ bootstrap/homebrew/brew.sh
@@ -9,40 +9,26 @@ brew upgrade 2>/dev/null
 brew tap homebrew/dupes 2>/dev/null
 
 # Development
+install_formula ack
+install_formula bash-completion
+install_formula chruby
+install_formula coreutils
+install_formula findutils
 install_formula git
+install_formula homebrew/dupes/grep
+install_formula homebrew/dupes/screen
+install_formula libyaml
 install_formula mysql
 install_formula openssl
 install_formula postgresql
+install_formula qt # --HEAD
 install_formula reattach-to-user-namespace
 install_formula redis
+install_formula ruby-install
 install_formula the_silver_searcher
 install_formula tmux
 
-# Ruby
-install_formula chruby
-install_formula ruby-install
-
-# Shell
-install_formula ack
-install_formula bash-completion
-install_formula coreutils
 e_arrow "Don't forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
-install_formula findutils
-install_formula homebrew/dupes/grep
-install_formula homebrew/dupes/screen
-
-# QuickTime
-echo
-echo "About to install QuickTime. This will take a LONG time."
-read -p "Are you sure you want to do this now? " -n 1 -r
-echo
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  install_formula qt --HEAD
-else
-  e_arrow "Skipping QuickTime"
-fi
 
 e_header "Cleaning up"
 brew cleanup
