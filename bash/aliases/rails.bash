a101ee3c251a64823475f16cf7d0fb2ddb462c2b
Tres Trantham <tres@trestrantham.com>
2013-11-30 00:13:21 -0500

    Add more database aliases

--- bash/aliases/rails.bash
+++ bash/aliases/rails.bash
@@ -1,5 +1,8 @@
 alias rc='rails c'
 alias rs='rails s'
 
+alias rdc='rake db:create'
 alias rdm='rake db:migrate db:test:prepare'
+alias rdsl='rake db:schema:load'
+alias rdtp='rake db:test:prepare'
 alias rgm='rails generate migration'
