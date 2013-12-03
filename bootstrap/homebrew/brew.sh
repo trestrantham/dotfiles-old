0213bb6076d55bb4d67e7b037f5a9d9e4d155ee7
Tres Trantham <tres@trestrantham.com>
2013-12-02 23:01:03 -0500

    Add gh to brew installs

--- bootstrap/homebrew/brew.sh
+++ bootstrap/homebrew/brew.sh
@@ -7,6 +7,7 @@ e_header "Setting up Homebrew Formulas"
 brew update 2>/dev/null
 brew upgrade 2>/dev/null
 brew tap homebrew/dupes 2>/dev/null
+brew tap jingweno/gh 2>/dev/null
 
 # Development
 install_formula ack
@@ -14,6 +15,7 @@ install_formula bash-completion
 install_formula chruby
 install_formula coreutils
 install_formula findutils
+install_formula gh
 install_formula git
 install_formula homebrew/dupes/grep
 install_formula homebrew/dupes/screen
