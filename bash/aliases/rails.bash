7b463cd1950f530d2a7803e10f4feaf16596c79a
Tres Trantham <tres@trestrantham.com>
2014-03-16 01:49:07 -0400

    Update aliases

--- bash/aliases/rails.bash
+++ bash/aliases/rails.bash
@@ -1,5 +1,6 @@
 alias rc='rails c'
 alias rs='rails s'
+alias rg='rails g'
 
 alias rdc='rake db:create'
 alias rdd='rake db:drop'
@@ -7,4 +8,9 @@ alias rdm='rake db:migrate db:test:prepare'
 alias rdsl='rake db:schema:load'
 alias rdtp='rake db:test:prepare'
 
-alias rgm='rails generate migration'
+alias rgm='rails generate model'
+alias rgmg='rails generate migration'
+alias rgc='rails generate controller'
+
+alias rerd='rake erd filetype=dot && dot -Tpdf erd.dot > erd.pdf && rm erd.dot && open erd.pdf'
+alias cov='COVERAGE=true rspec && open coverage/index.html'
