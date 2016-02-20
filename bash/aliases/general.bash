272883f8bcdfece3e986fd7f7a9d4a331240a81c
Tres Trantham <tres@trestrantham.com>
2016-02-19 19:53:45 -0800

    Add stats aliases

--- bash/aliases/general.bash
+++ bash/aliases/general.bash
@@ -14,7 +14,13 @@ alias vim='nvim'
 
 alias dbmg="[ -f mix.exs ] && mix ecto.migrate || rake db:migrate"
 alias dbrb="[ -f mix.exs ] && mix ecto.rollback || rake db:rollback"
+
 alias console="[ -f mix.exs ] && iex -S mix || bundle exec rails c"
 alias con="console"
+
 alias server="[ -f mix.exs ] && mix phoenix.server || bundle exec rails s"
 alias serv="server"
+
+alias stats="[ -f mix.exs ] && mix credo -a"
+alias statss="stats --strict"
+alias statso="stats --one-line"
