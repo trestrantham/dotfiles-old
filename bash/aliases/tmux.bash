e3a15aa7b2ffb9aa9ab6a4183f47bf7d7da5498b
Tres Trantham <tres@trestrantham.com>
2016-04-26 22:39:51 -0700

    Remove socket tmux config

--- bash/aliases/tmux.bash
+++ bash/aliases/tmux.bash
@@ -3,10 +3,3 @@ alias t='tmux'
 alias tn='t new-session -s'
 alias ta='t attach -t'
 alias tls='t list-sessions'
-
-# Socket tmux
-alias T='tmux -S ~/.tmux.socket'
-alias Tn='T new -s'
-alias Ta='T attach -t'
-alias Tnp='ssh-agent Tn pair'
-alias Tls='T list-sessions'
