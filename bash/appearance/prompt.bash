3e4e57629ed6c7266d9410a851215d908d8116b9
Tres Trantham <tres@trestrantham.com>
2017-03-24 12:23:11 -0400

    Add time to prompt

--- bash/appearance/prompt.bash
+++ bash/appearance/prompt.bash
@@ -34,7 +34,7 @@ bash_prompt() {
   local UC=$W                 # user's color
   [ $UID -eq "0" ] && UC=$R   # root's color
 
-  PS1="\n$B\w$C\$(__git_ps1)\n$G"λ"${NONE} "
+  PS1="\n$B\w $K[\D{%-l:%M}]$C\$(__git_ps1)\n$G"λ"${NONE} "
 }
 
 bash_prompt
